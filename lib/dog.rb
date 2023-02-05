7189e76060d113ede606b8c55973eb2e72c9e6d2
Teanna <teannamtaylor95@gmail.com>
2023-02-05 12:01:23 -0800

    commit tree 316fa5801126b807f14dc39db1dc62535a359217

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
-
+    #Have Defined Two Instance Methods;
+    #This first one takes an argument of a dog's name
+    #and assigns that value to a variable
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end 
+    #The second method is responsible for reporting the name
+    def name
+        @this_dogs_name
+    end 
 end
+
+lassie = Dog.new
+lassie.name ="Lassie"
+lassie.name
